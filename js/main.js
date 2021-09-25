779e0204d5c0eb2a0755749adf9d4f51938c69fc
Kate <skoropeya@inbox.ru>
2021-09-25 22:53:59 +0300

    В разметке подключен скрипт, добавлена функция, выводящая случайное число в заданном диапазоне

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function checkPositive(num) {
+  return (num >= 0);
+}
+
+function getRandom(min, max, decimalPlaces) {
+
+  if (!checkPositive(min) || !checkPositive(max) || !checkPositive(decimalPlaces)) {
+    return 'Введено отрицательное значение, функция не может быть выполнена';
+  }
+
+  // Если значение «до» равно значению «от», выведем это значение,
+  // так как других чисел в таком диапазоне быть не может
+  if (min === max) {
+    return min;
+  }
+
+  // Если значение «до» меньшее, чем значение «от», выведем число, находящееся между этими значениями
+  // Для этого поменяем местами min и max
+  if (min > max) {
+    const swaper = min;
+    min = max;
+    max = swaper;
+  }
+
+  return (min + Math.random() * (max - min)).toFixed(decimalPlaces);
+}
+
+getRandom();
