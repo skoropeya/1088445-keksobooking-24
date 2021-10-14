a89f34577188905d818fbe137fda5795178ae0db
Kate <skoropeya@inbox.ru>
2021-10-14 12:21:54 +0300

    Добавила проверк на наличие данных, соответсвующие блоки скрываются, если данных нет

--- js/generate-elements.js
+++ js/generate-elements.js
@@ -3,9 +3,6 @@ import {createOffer} from './utils/create-offer.js';
 const OFFERS_COUNT = 10;
 const offers = Array.from({length: OFFERS_COUNT}, createOffer);
 
-const canvas = document.querySelector('#map-canvas');
-canvas.style.height = 'auto';
-
 const templateCard = document.querySelector('#card').content;
 const offerFragment = document.createDocumentFragment();
 
@@ -20,39 +17,60 @@ const TYPES = {
 offers.forEach((offerItem) => {
   const offerElement = templateCard.cloneNode(true);
 
-  offerElement.querySelector('.popup__avatar').src = offerItem.author.avatar;
-  offerElement.querySelector('.popup__title').textContent = offerItem.offer.title;
-  offerElement.querySelector('.popup__text--address').textContent = offerItem.offer.address;
-  offerElement.querySelector('.popup__text--price').textContent = `${offerItem.offer.price  } ₽/ночь`;
-  offerElement.querySelector('.popup__type').textContent = TYPES[offerItem.offer.type];
-  offerElement.querySelector('.popup__text--capacity').textContent = `${offerItem.offer.rooms} комнаты для ${offerItem.offer.guests} гостей`;
-  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${offerItem.offer.checkin}, выезд до ${offerItem.offer.checkout}`;
-  offerElement.querySelector('.popup__description').textContent = offerItem.offer.description;
+  const avatar = offerElement.querySelector('.popup__avatar');
+  const title = offerElement.querySelector('.popup__title');
+  const address = offerElement.querySelector('.popup__text--address');
+  const price = offerElement.querySelector('.popup__text--price');
+  const type = offerElement.querySelector('.popup__type');
+  const capacity = offerElement.querySelector('.popup__text--capacity');
+  const time = offerElement.querySelector('.popup__text--time');
+  const description = offerElement.querySelector('.popup__description');
+  const featuresList = offerElement.querySelector('.popup__features');
+  const photosList = offerElement.querySelector('.popup__photos');
+
+  offerItem.author.avatar ? avatar.src = offerItem.author.avatar : avatar.classList.add('hidden');
+  offerItem.offer.title ? title.textContent =  offerItem.offer.title : title.classList.add('hidden');
+  offerItem.offer.address ? address.textContent =  offerItem.offer.address : address.classList.add('hidden');
+  offerItem.offer.price ? price.textContent =  `${offerItem.offer.price  } ₽/ночь` : price.classList.add('hidden');
+  offerItem.offer.type ? type.textContent =  TYPES[offerItem.offer.type] : type.classList.add('hidden');
+  offerItem.offer.description ? description.textContent = offerItem.offer.description : description.classList.add('hidden');
+  (offerItem.offer.rooms & offerItem.offer.guests) ? capacity.textContent = `${offerItem.offer.rooms} комнаты для ${offerItem.offer.guests} гостей` : capacity.classList.add('hidden');
+  (offerItem.offer.checkin & offerItem.offer.checkout) ? time.textContent = `${offerItem.offer.checkin} комнаты для ${offerItem.offer.checkout} гостей` : time.classList.add('hidden');
 
   const featuresArray = offerItem.offer.features;
-  const featuresList = offerElement.querySelector('.popup__features');
-  const futuresFragment = document.createDocumentFragment();
+  if (featuresArray.length === 0) {
+    featuresList.classList.add('hidden');
+  } else {
+    const futuresFragment = document.createDocumentFragment();
 
-  featuresArray.forEach((future) => {
-    const futureItem = featuresList.querySelector(`.popup__feature--${  future}`);
-    if (futureItem) {
-      futuresFragment.appendChild(futureItem);
-    }
-  });
-  featuresList.innerHTML = '';
-  featuresList.appendChild(futuresFragment);
+    featuresArray.forEach((future) => {
+      const futureItem = featuresList.querySelector(`.popup__feature--${future}`);
+      if (futureItem) {
+        futuresFragment.appendChild(futureItem);
+      }
+    });
+    featuresList.innerHTML = '';
+    featuresList.appendChild(futuresFragment);
+  }
 
   const photosArray = offerItem.offer.photos;
-  const photosList = offerElement.querySelector('.popup__photos');
-  const photoTemplate = photosList.querySelector('.popup__photo');
-  photosList.innerHTML = '';
-  photosArray.forEach((photo) => {
-    const photoItem = photoTemplate.cloneNode(false);
-    photoItem.src = photo;
-    photosList.appendChild(photoItem);
-  });
+  if (photosArray.length === 0) {
+    photosList.classList.add('hidden');
+  } else {
+    const photoTemplate = photosList.querySelector('.popup__photo');
+    photosList.innerHTML = '';
+    photosArray.forEach((photo) => {
+      const photoItem = photoTemplate.cloneNode(false);
+      photoItem.src = photo;
+      photosList.appendChild(photoItem);
+    });
+  }
 
   offerFragment.appendChild(offerElement);
 });
 
-canvas.appendChild(offerFragment);
+
+const canvas = document.querySelector('#map-canvas');
+// canvas.style.height = 'auto';
+// canvas.appendChild(offerFragment);
+canvas.appendChild(offerFragment.children[0]);
